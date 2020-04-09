4530bf4b3559fa574b7c967ae4b846fa03f63ca9
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-09 15:22:19 -0300

    add app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-const soma = (a, b) => a + b;
+import React from 'react';
 
-alert(soma(1,4))
+import { render } from 'react-dom';
+
+import App from  './App';
+
+render(<App />, document.getElementById('app'))
